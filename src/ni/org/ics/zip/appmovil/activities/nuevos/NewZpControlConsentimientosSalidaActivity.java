42f01f68e6d2fe289d51c993c04ab91d11eafdcc
William Aviles Monterrey <waviles@icsnicaragua.org>
2016-12-13 09:12:38 -0600

    Cambios envio y recepcion de consentimientos y ultrasonidos

--- src/ni/org/ics/zip/appmovil/activities/nuevos/NewZpControlConsentimientosSalidaActivity.java
+++ src/ni/org/ics/zip/appmovil/activities/nuevos/NewZpControlConsentimientosSalidaActivity.java
@@ -2,7 +2,11 @@ package ni.org.ics.zip.appmovil.activities.nuevos;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.ListIterator;
+
 import ni.org.ics.zip.appmovil.AbstractAsyncActivity;
 import ni.org.ics.zip.appmovil.MainActivity;
 import ni.org.ics.zip.appmovil.MyZipApplication;
@@ -18,6 +22,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -25,6 +30,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +38,7 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.AlertDialog;
@@ -45,14 +52,19 @@ public class NewZpControlConsentimientosSalidaActivity extends AbstractAsyncActi
 
 	private Spinner mLugarView;
 	private Spinner mPersonaView;
+	private Spinner mMetodoView;
 	private EditText mCodigoView;
+	private TextView mLabelCodigoView;
 	private Button mSaveButton;
 	private Button mFinishButton;
 	private ImageButton mBarcodeButton;
 	private String mCodigo;
 	private Date todayWithZeroTime = null;
+	private String mCodigosDeHoy;
 	
 	private static final int EXIT = 1;
+	private static final int HOME = 0;
+	private static final int REVIEW = 3;
 	private static final int BARCODE_CAPTURE = 2;
 	
 	private AlertDialog alertDialog;
@@ -95,18 +107,52 @@ public class NewZpControlConsentimientosSalidaActivity extends AbstractAsyncActi
 		mLugarView = (Spinner) findViewById(R.id.lugar);
 		mPersonaView = (Spinner) findViewById(R.id.persona);
 		mCodigoView = (EditText) findViewById(R.id.codigo);
+		mCodigoView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+		mLabelCodigoView = (TextView) findViewById(R.id.label_codigo);
 		
-		String[] list_lugares = getResources().getStringArray(R.array.list_lugares);
+		String[] list_lugares = getResources().getStringArray(R.array.list_lugares_salida);
 		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_spinner_item, list_lugares);
 		dataAdapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
 		mLugarView.setAdapter(dataAdapter);
 		
-		String[] list_personas = getResources().getStringArray(R.array.list_drivers);
+		String[] list_personas = getResources().getStringArray(R.array.list_personas_salida_cons);
 		ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
 				android.R.layout.simple_spinner_item, list_personas);
 		dataAdapter2.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
 		mPersonaView.setAdapter(dataAdapter2);
+		
+		mMetodoView = (Spinner) findViewById(R.id.metodo_busqueda);
+		List<String> list = new ArrayList<String>();
+		list.add(getString(R.string.desc_barcode));
+		list.add(getString(R.string.enter)+" "+getString(R.string.mat_id));
+		ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, list);
+		dataAdapter3.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+		mMetodoView.setAdapter(dataAdapter3);
+
+		mMetodoView.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+				if (position==0){
+					mCodigoView.setEnabled(false);
+					mBarcodeButton.setVisibility(View.VISIBLE);
+					mCodigoView.setHint(getString(R.string.desc_barcode));
+					mLabelCodigoView.setText(R.string.desc_barcode);
+					mCodigoView.setText(null);
+				}
+				else{
+					mCodigoView.setEnabled(true);
+					mBarcodeButton.setVisibility(View.GONE);
+					mCodigoView.setHint(getString(R.string.mat_id));
+					mLabelCodigoView.setText(getString(R.string.enter)+" "+getString(R.string.mat_id));
+				}
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parentView) {
+
+			}
+		});
 
 		mSaveButton = (Button) findViewById(R.id.saveButton);
 		mSaveButton.setText(R.string.save);
@@ -114,6 +160,7 @@ public class NewZpControlConsentimientosSalidaActivity extends AbstractAsyncActi
 		mSaveButton.setOnClickListener(new View.OnClickListener()  {
 			@Override
 			public void onClick(View v) {
+				mCodigo = mCodigoView.getText().toString();
 				if(validarEntrada()){
 					new SaveDataTask().execute(mLugarView.getSelectedItem().toString(),
 							mPersonaView.getSelectedItem().toString(),
@@ -163,7 +210,13 @@ public class NewZpControlConsentimientosSalidaActivity extends AbstractAsyncActi
 			return false;
 		}
 		if (mCodigo==null){
-			showToast(this.getString( R.string.scan_error));
+			showToast(this.getString( R.string.code_error));
+			return false;
+		}
+		if(!(mCodigo.matches("^07[0-9][0-9][0-9][0-9][0-3][A-Y]$"))){
+			showToast(mCodigo + " " + getString(R.string.code_error));
+			mCodigoView.setText(null);
+			mCodigo=null;
 			return false;
 		}
 		else{
@@ -190,26 +243,22 @@ public class NewZpControlConsentimientosSalidaActivity extends AbstractAsyncActi
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
-		getMenuInflater().inflate(R.menu.general, menu);
+		getMenuInflater().inflate(R.menu.control, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		if(item.getItemId()==android.R.id.home){
-			createDialog(EXIT);
-			return true;
-		}
-		else if(item.getItemId()==R.id.MENU_BACK){
+		if(item.getItemId()==R.id.MENU_BACK){
 			createDialog(EXIT);
 			return true;
 		}
 		else if(item.getItemId()==R.id.MENU_HOME){
-			Intent i = new Intent(getApplicationContext(),
-					MainActivity.class);
-			i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(i);
-			finish();
+			createDialog(HOME);
+			return true;
+		}
+		else if(item.getItemId()==R.id.MENU_VIEW){
+			new GetDataTask().execute();
 			return true;
 		}
 		else{
@@ -244,6 +293,38 @@ public class NewZpControlConsentimientosSalidaActivity extends AbstractAsyncActi
 				}
 			});
 			break;
+		case HOME:
+			builder.setTitle(this.getString(R.string.confirm));
+			builder.setMessage(this.getString(R.string.exiting));
+			builder.setPositiveButton(this.getString(R.string.yes), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					// Finish app
+					dialog.dismiss();
+					zipA.close();
+					Intent i = new Intent(getApplicationContext(),
+							MainActivity.class);
+					i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					startActivity(i);
+					finish();
+				}
+			});
+			builder.setNegativeButton(this.getString(R.string.no), new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// Do nothing
+					dialog.dismiss();
+				}
+			});
+			break;
+		case REVIEW:
+			builder.setTitle(this.getString(R.string.review));
+			builder.setMessage(mCodigosDeHoy);
+			builder.setPositiveButton(this.getString(R.string.ok), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+			break;			
 		default:
 			break;
 		}
@@ -350,6 +431,51 @@ public class NewZpControlConsentimientosSalidaActivity extends AbstractAsyncActi
 		}
 	}	
 
+	// ***************************************
+	// Private classes
+	// ***************************************
+	private class GetDataTask extends AsyncTask<String, Void, List<ZpControlConsentimientosSalida>> {
+		private List<ZpControlConsentimientosSalida> zpListaDeHoy = null;
+		@Override
+		protected void onPreExecute() {
+			// before the request begins, show a progress indicator
+			showLoadingProgressDialog();
+		}
+
+		@Override
+		protected List<ZpControlConsentimientosSalida> doInBackground(String... values) {
+			try {
+				zpListaDeHoy = zipA.getZpControlConsentimientosSalidas(MainDBConstants.fechaHoraSalida + "=" + todayWithZeroTime.getTime(), MainDBConstants.codigo);
+				return zpListaDeHoy;
+			} catch (Exception e) {
+				Log.e(TAG, e.getLocalizedMessage(), e);
+				return null;
+			}
+		}
+
+		protected void onPostExecute(List<ZpControlConsentimientosSalida> resultado) {
+			// after the network request completes, hide the progress indicator
+			dismissProgressDialog();
+			showResultados(resultado);
+		}
+
+	}
 	
+	// ***************************************
+	// Private methods
+	// ***************************************
+	private void showResultados(List<ZpControlConsentimientosSalida> resultado) {
+		if(resultado!=null){
+			mCodigosDeHoy = this.getString(R.string.today_items) + ": " + String.valueOf(resultado.size()) + "\n";
+			ListIterator<ZpControlConsentimientosSalida> iter = resultado.listIterator();
+            while (iter.hasNext()){
+            	mCodigosDeHoy = mCodigosDeHoy + iter.next().getCodigo()+ "\n";
+            }
+		}
+		else{
+			mCodigosDeHoy = this.getString(R.string.no_items);
+		}
+		createDialog(REVIEW);
+	}
 	
 }
